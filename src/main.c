06e651bba87d9c61f2566fe2a800317aefc8f8c4
rahvax <gabriel@caelestis.one>
2026-08-31 17:55:36 -0300

    Teste da função checkFile

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include "../include/vaxcore/vaxcore.h"
+#include <stdio.h>
 
 int main(void) {
+  char buffer[] = "Mundo";
+  logMessage(Sucess, "Olá, %s!", buffer);
 
+  if (checkFile("maakefile") == -1) {
+    printf("F\n");
+    return Error;
+  }
+  
   return Sucess;
 }
